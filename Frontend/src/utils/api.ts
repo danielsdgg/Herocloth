bfd51e7e6f3f7129b5f18dd8463d5c651ce91b6e
Daniel Muiruri <gathigidg26@gmail.com>
2025-11-02 13:46:03 +0300

    changes

--- Frontend/src/utils/api.ts
+++ Frontend/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const createApiInstance = (token: string | null) => {
   const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_URL || "https://herocloth.onrender.com/",
     headers: {
       "Content-Type": "application/json",
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
