de28cf70d8cec5c339ad92cc6e76d66c34aca8ba
Daniel Muiruri <gathigidg26@gmail.com>
2025-11-02 13:13:33 +0300

    upd

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
